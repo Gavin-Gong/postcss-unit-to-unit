e57df65f5637e54cd0ac1433729c625d808c0c2f
Gavin Gong <kefengong@gmail.com>
2018-05-21 13:51:26 +0800

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+const postcss = require('postcss');
+
+module.exports = postcss.plugin('postcss-unit2unit', function (opts = {}) {
+    opts = Object.assign({}, {
+        from: 'px',
+        to: 'px',
+        multiple: 1,
+        decimalPlaces: 0,
+        skip: 'skip',
+    }, opts)
+
+    function replace(str) {
+        if (!str) return str;
+
+        const regexpStr = `([0-9.]+)(${opts.from})$`;
+        const regexp = new RegExp(regexpStr, 'gi');
+
+        return str.replace(new RegExp(regexp), (match, num) => {
+            let finalNum = (num * opts.multiple).toFixed(opts.decimalPlaces);
+            let finalUnit = opts.to;
+            return `${finalNum}${finalUnit}`;
+        });
+    }
+
+    return function (root) {
+        root.walkDecls(decl => {
+            if (decl && decl.next() && decl.next().type === 'comment' && decl.next().text === opts.skip) {
+                decl.next().remove();
+            } else {
+                decl.value = replace(decl.value);
+            }
+        });
+    };
+});
---
+++ index.test.js
@@ -0,0 +1,48 @@
+/* eslint:disable */
+const postcss = require('postcss');
+
+const plugin = require('./');
+
+function run(input, output, opts) {
+    return postcss([ plugin(opts) ]).process(input)
+        .then(result => {
+            expect(result.css).toEqual(output);
+            expect(result.warnings().length).toBe(0);
+        });
+}
+
+
+it('test replace', () => {
+    return run(
+        'a{margin:10rpx}',
+        'a{margin:10rem}',
+        { from: 'rpx', to: 'rem' }
+    );
+});
+it('test multiple', () => {
+    return run(
+        'a{margin:10rpx}',
+        'a{margin:20rem}',
+        { from: 'rpx', to: 'rem', multiple: 2 }
+    );
+});
+it('test decimalPlaces', () => {
+    return run(
+        'a{margin:10rpx}',
+        'a{margin:20.00rem}',
+        { from: 'rpx', to: 'rem', multiple: 2, decimalPlaces: 2 }
+    );
+});
+it('test skip', () => {
+    return run(
+        'a{margin:10rpx /* skip */}',
+        'a{margin:10rpx}',
+        { from: 'rpx', to: 'rem', skip: 'skip' });
+});
+it('test user case', () => {
+    return run(
+        'a{margin:100rpx}',
+        'a{margin:8.5333rem}',
+        { from: 'rpx', to: 'rem', multiple: 0.085332969, decimalPlaces: 4 });
+});
+
